43c97057e7b615016bc5f1a8b81bdeca6f0e90af
Chrysa Lazaridou <chrysanthi.lazaridou@gmail.com>
2021-06-04 14:59:31 +0300

    error messages and patchValue application

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
@@ -16,34 +16,49 @@ export class FormComponent implements OnInit {
   formData = new Subject();
   show = false;
   data;
+  idValue: string
+  nameChanges = new BehaviorSubject<boolean>(false)
 
-  genders: string[] = ['female','male']
-  ageOptions:string [] = ['under 18','18-35','36-50', '50 +']
+  genders: string[] = ['female', 'male']
+  ageOptions: string[] = ['under 18', '18-35', '36-50', '50 +']
   constructor(
-    private fb:FormBuilder
+    private fb: FormBuilder
   ) { }
 
+
   ngOnInit(): void {
-    this.myForm = new FormGroup ({
-      'personalInformation' : new FormGroup ({
-        'firstName': new FormControl(null),
-        'lastName': new FormControl(null),
+
+    this.idValue = Math.random().toPrecision(5)
+
+    this.myForm = new FormGroup({
+      'personalInformation': new FormGroup({
+        'id': new FormControl(this.idValue, [Validators.required]),
+        'firstName': new FormControl(null, [Validators.required,Validators.maxLength(10)]),
+        'lastName': new FormControl(null, [Validators.minLength(3)]),
         'age': new FormControl(null)
       }),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('female'),
-      'genderChoice' : new FormControl(null),
+      'genderChoice': new FormControl(null),
       'foods': new FormArray([])
     })
+    // },{updateOn: 'submit'}) // otan thelw na ginetai to validation sto submit. ! to submit ginetai kanonika
 
 
-    this.myForm.get('personalInformation.firstName').valueChanges.pipe(debounceTime(1000)).subscribe( fN => console.log(fN))
+    console.log("pristine",this.myForm.get('personalInformation.firstName').pristine)
+    this.myForm.get('personalInformation.firstName').valueChanges.pipe(debounceTime(1000)).subscribe(value => {
 
-    // this.myForm.patchValue({
-    //   'personalInformation' : {
-    //     'firstName': 'Anna',
-    //     'lastName': 'Papadopoulou'
-    //   }
-    // })
+      if (this.myForm.get('personalInformation.firstName').valid && !this.myForm.get('personalInformation.id').touched  ) {
+        this.idValue += value
+        this.nameChanges.next(true)
+
+        console.log(this.idValue)
+      }
+
+    })
+
+    this.nameChanges.subscribe((val) => val ? this.changeId : false)
+
 
     // this.myForm.setValue({
     //   'personalInformation' :{
@@ -57,19 +72,29 @@ export class FormComponent implements OnInit {
     // })
   }
 
+
   get foods() {
     return this.myForm.controls["foods"] as FormArray;
   }
 
+  get changeId() {
+    return this.myForm.patchValue({
+      'personalInformation': {
+        'id': this.idValue
+      }
+    })
+  }
+
   onAddFood() {
-    const control= new FormControl (null,Validators.required);
+    const control = new FormControl(null, Validators.required);
     this.foods.push(control);
 
   }
 
   onSubmit() {
 
-    console.log("form",this.myForm.value)
+    console.log("form", this.myForm)
+    // this.myForm.reset()
   }
 
 }
